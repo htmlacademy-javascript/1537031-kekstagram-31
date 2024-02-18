d6c407503c2d8f2e61133d328ad2fa6223e9de3d
Anna <anna.mail787@gmail.com>
2024-02-18 13:43:29 +0000

    helper functions

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+const isStringShorter = (str, maxLength) => {
+  return str.length <= maxLength;
+};
+
+isStringShorter('проверяемая строка', 20);
+isStringShorter('проверяемая строка', 18);
+isStringShorter('проверяемая строка', 10);
+
+const isPalindrome = (str) => {
+
+  const modifiedString = str.replaceAll(' ', '').toLowerCase();
+  let result = '';
+  let i = str.length - 1;
+  while (i >= 0) {
+    result = result + modifiedString.at(i);
+    i--;
+  }
+  return modifiedString === result;
+};
+
+isPalindrome('топот');
+isPalindrome('ДовОд');
+isPalindrome('Кекс');
